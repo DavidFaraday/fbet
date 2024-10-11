f4ac463a58d4eafa8d3300aa0e1db6aa6a9aeee7
DavidFaraday <iosdevschool@gmail.com>
2024-10-11 15:56:40 +0300

    Initial Commit

---
+++ Fonbet/FonbetApp.swift
@@ -0,0 +1,17 @@
+//
+//  FonbetApp.swift
+//  Fonbet
+//
+//  Created by David Kababyan on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct FonbetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FonbetTests/FonbetTests.swift
@@ -0,0 +1,17 @@
+//
+//  FonbetTests.swift
+//  FonbetTests
+//
+//  Created by David Kababyan on 11/10/2024.
+//
+
+import Testing
+@testable import Fonbet
+
+struct FonbetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
